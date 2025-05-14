658e859c168c6971d6480f55685da6fa188417ae
jeevanbarnana <barnanajeevanrao@gmail.com>
2025-05-14 20:11:42 +0530

    Create 27-remove element.leetcode.cpp

---
+++ 27-remove element.leetcode.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n=nums.size();
+        int res=0;
+       for(int i=0;i<n;i++)
+       {
+        if(nums[i]!=val)
+        {
+          nums[res]=nums[i];
+          res++;
+        }    
+       }
+       return res;
+    }
+};
