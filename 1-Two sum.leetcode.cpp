4d455a874a5118d8c3729db67df76cbacfef8dcd
jeevanbarnana <barnanajeevanrao@gmail.com>
2025-05-15 07:35:06 +0530

    Create 1-Two sum.leetcode.cpp

---
+++ 1-Two sum.leetcode.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            int result=target-nums[i];
+            if(mp.count(result))
+                return {mp[result],i};
+            mp[nums[i]]=i;
+        }
+        return {};
+        }
+};
